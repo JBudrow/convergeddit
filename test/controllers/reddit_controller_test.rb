b96a3621d72a008662f7906b3ad433a7bf3186fc
jbudrow <jbudrowd3@yahoo.com>
2016-04-02 17:27:02 -0400

    add user and reddit controllers

--- test/controllers/reddit_controller_test.rb
+++ test/controllers/reddit_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class RedditControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
