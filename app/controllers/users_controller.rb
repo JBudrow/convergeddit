d71a2694cf65569e85be67faa8ac4b4cc583cf58
jbudrow <jbudrowd3@yahoo.com>
2016-04-03 15:51:14 -0400

    add register user functionality via mvc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    # @user = User.find(params['id'])
+  end
+
   def new
+    @user = User.new
+    render 'new'
+  end
+
+  def create
+    @user = User.new(name: params['name'],
+                     email: params['email'],
+                     password: params['password'])
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 end
