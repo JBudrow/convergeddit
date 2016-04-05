b12ae93498b1efc31bbd266fac9dfa8030198224
jbudrow <jbudrowd3@yahoo.com>
2016-04-05 15:29:45 -0400

    fix incorrect user password parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params['email'])
-    if @user && @user.authenticate(params['passwordrail'])
+    if @user && @user.authenticate(params['password'])
       session[:user_id] = @user.id
       flash[:notice] = "Logged in."
       redirect_to root_url
