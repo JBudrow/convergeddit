dbcd13a39fc1832dd35afbc778167c979e437677
jbudrow <jbudrowd3@yahoo.com>
2016-04-11 19:43:56 -0400

    create authorize! method for user registry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :logged_in_user
 
@@ -8,6 +6,13 @@ class ApplicationController < ActionController::Base
     user_id = session['user_id']
     if user_id
       User.find_by(id: user_id)
+    end
+  end
+
+  def authenticate!
+    unless logged_in_user
+      flash[:notice] = "must be logged in"
+      redirect_to root_url
     end
   end
 end
