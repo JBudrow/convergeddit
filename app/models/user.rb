6c6d30984987237268addf6a730749620b400a41
jbudrow <jbudrowd3@yahoo.com>
2016-04-11 19:56:11 -0400

    add travis.yml file, referenced it in README.md, remove garbage fixtures and controller tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email
   validates :email, format: {
-              with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
+              with: /.+@.+\..+/,  message: 'Must have an @ symbol and a period.'
             }
   has_many :links
 end
