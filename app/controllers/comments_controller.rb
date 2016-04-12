bdf05e8f51f8b99db2f380752a310bb9707bf3f2
jbudrow <jbudrowd3@yahoo.com>
2016-04-11 23:32:13 -0400

    title validation test for links model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    # binding.pry
     @link = Link.find_by(params['id'])
     @link.comments.create(body: params['body'],
                           user_id: logged_in_user.id)
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save link without title' do
+    link = Link.new
+    assert_not link.save
+  end
 end
