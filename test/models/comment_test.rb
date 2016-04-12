bdd329887b0d53a485e3ae07f21dc07957efe791
jbudrow <jbudrowd3@yahoo.com>
2016-04-12 00:06:43 -0400

    parameter validation for comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Comment saves with all parameters' do
+    comment = Comment.create(body: 'Comment body', user_id: 3)
+    assert comment.valid?, 'Comment did not save'
+  end
 end
