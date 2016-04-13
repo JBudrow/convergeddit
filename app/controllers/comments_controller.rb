54f3319a1f3e7b1f7c7280cc39dd207459e55e82
jbudrow <jbudrowd3@yahoo.com>
2016-04-13 13:20:14 -0400

    refactor comments create with strong params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @link = Link.find_by(params['id'])
-    @link.comments.create(body: params['body'],
-                          user_id: logged_in_user.id)
+    @link.comments.create(comment_params)
     redirect_to link_path(@link)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comments).permit(:body, :user_id)
   end
 end
