a03c06cf671f1e49d6dcc009dae3376ba5e6a55a
jbudrow <jbudrowd3@yahoo.com>
2016-04-13 13:03:04 -0400

    add two tests for sessions new/create

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+  test 'should render new' do
+    get :new
+    assert_template :new
+  end
+
+  test 'should create users session' do
+    post :create, {email: users(:tycho).email, password: 'space'}
+    assert_equal session[:user_id], users(:tycho).id
+    assert_redirected_to root_url
+  end
+
   test 'should destroy users session' do
     delete :destroy
     assert_redirected_to root_path
