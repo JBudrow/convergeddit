d7adb556bf14935bb6ef368e51ee223396c25c6d
jbudrow <jbudrowd3@yahoo.com>
2016-04-13 14:29:06 -0400

    fix minor bug in user controller/test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.permit(:name, :email, :password)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,17 +8,17 @@ class UsersControllerTest < ActionController::TestCase
 
   test 'should create user' do
     assert_difference('User.count') do
-      post :create, user: {name: 'ntyson',
-                           email: 'ntyson@domain.com',
-                           password: 'ntyson'}
+      post :create, {name: 'ntyson',
+                     email: 'ntyson@domain.com',
+                     password: 'ntyson'}
     end
     assert_redirected_to root_path
   end
 
   test 'should not create user with invalid information' do
-     post :create, user: {name: '',
-                          email: 'noname.com',
-                          password: '3'}
+     post :create, {name: '',
+                    email: 'noname.com',
+                    password: '3'}
      assert_template :new
   end
 end
