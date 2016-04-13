8a7de5dd853d90bf8fef23521feb4673757cb916
jbudrow <jbudrowd3@yahoo.com>
2016-04-13 09:06:01 -0400

    tweak user/link controller and views

---
+++ test/controllers/links_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class LinksControllerTest < ActionController::TestCase
+  def logged_in(user)
+    session[:user_id] = users(user).id
+  end
+
+  test "should get new" do
+    get :new
+    assert_redirected_to root_path
+  end
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:links)
+  end
+
+  test 'should let signed in users view link form' do 
+    logged_in(:tycho)
+    get :new
+    assert_template :new
+    assert_response :ok
+  end
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,19 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should create user' do
+    assert_difference('User.count') do
+      post :create, {name: 'ntyson',
+                     email: 'ntyson@domain.com',
+                     password: 'ntyson'}
+    end
+    assert_redirected_to root_path
+  end
+
+  test 'should not create user with invalid information' do
+     post :create, {name: '',
+                    email: 'noname.com',
+                    password: '3'}
+     assert_template :new
+  end
 end
