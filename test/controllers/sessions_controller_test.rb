396563b60c72bdfd571284e7b6e03de099cf1a9b
jbudrow <jbudrowd3@yahoo.com>
2016-04-13 09:02:15 -0400

    create session controller, add test to destroy a users session

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
+  test 'should destroy users session' do
+    delete :destroy
+    assert_redirected_to root_path
+    assert_nil session[:user_id]
   end
-
 end
