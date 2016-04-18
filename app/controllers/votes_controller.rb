8ed4edd0b6b199ae6de57d6f14afe2ae5801f095
jbudrow <jbudrowd3@yahoo.com>
2016-04-17 20:04:10 -0400

    remove commented out er, comment

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,6 @@ class VotesController < ApplicationController
     @vote = logged_in_user.votes.new(link_id: params[:link_id],
                                      score: params[:score])
     flash[:notice] = 'unable to do that action' unless @vote.save
-    redirect_to root_path # default GET
+    redirect_to root_path 
   end
 end
