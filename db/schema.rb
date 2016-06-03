620b910c55d500bebcd448e71c16fc1a0ec9fb78
jbudrow <jbudrowd3@yahoo.com>
2016-06-03 17:49:38 -0400

    design overhaul in progress, using bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413172645) do
+ActiveRecord::Schema.define(version: 20160418220651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 20160413172645) do
   create_table "links", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
-    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "address"
   end
 
   create_table "users", force: :cascade do |t|
